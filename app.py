389a651ff99e3a5a5025ea3e208f821d2cae8191
Madhava Di Florio <madhava.di-florio@etu.univ-lyon1.fr>
2020-05-27 14:26:41 +0000

    test env

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import os
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return 'Hello Devops2 production branch !'
+    return os.getenv("DB_PASS")
+    # return 'Hello Devops2 production branch !'
 
 @app.route("/health")
 def health():
